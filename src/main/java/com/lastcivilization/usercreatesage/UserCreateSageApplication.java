f2ce90d182648ff84c0c060cabf46f96c4f518d9
mrrevo55 <kwolny31@gmail.com>
2022-10-03 16:31:59 +0200

    Initial commit

---
+++ src/main/java/com/lastcivilization/usercreatesage/UserCreateSageApplication.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.usercreatesage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCreateSageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCreateSageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lastcivilization/usercreatesage/UserCreateSageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.usercreatesage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCreateSageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
