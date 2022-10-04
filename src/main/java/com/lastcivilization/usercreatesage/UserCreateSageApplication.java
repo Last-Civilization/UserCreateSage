488275b192ef857cbf0bfe58553acfcdbcf421da
mrrevo55 <kwolny31@gmail.com>
2022-10-04 18:41:21 +0200

    Add eureka and feign

--- src/main/java/com/lastcivilization/usercreatesage/UserCreateSageApplication.java
+++ src/main/java/com/lastcivilization/usercreatesage/UserCreateSageApplication.java
@@ -2,8 +2,12 @@ package com.lastcivilization.usercreatesage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients(basePackages = "com.lastcivilization.usercreatesage.feign")
 public class UserCreateSageApplication {
 
 	public static void main(String[] args) {
